ca1006575130b710f118202e786d77f723b12e54
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-08-06 14:14:32 -0700

    Update cta.js

--- components/cta.js
+++ components/cta.js
@@ -7,7 +7,8 @@ const Cta = () => {
       <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
           <h2 className="text-2xl font-medium lg:text-3xl">
-            K.i.t.e.NFT Mint
+            K.i.t.e. NFT Collection Mint... LIVE
+            Pilot Coin "High Flyers" Coin Mint...LIVE
           </h2>
           <p className="mt-2 font-medium text-white text-opacity-90 lg:text-xl">
             Limited Collection Digital Pilot K.i.t.e.NFT's On The Solana Blockchain
@@ -45,6 +46,7 @@ const Cta = () => {
 }
 
 export default Cta;
+
 
 
 
