767755e55454ac1811e3cbc5f1d4eb9a03562368
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-08-06 16:08:03 -0700

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 import ThemeChanger from "./DarkSwitch";
 import Image from "next/image";
 import { Disclosure } from "@headlessui/react";
+import { useRouter } from "next/router";
 
-// Navigation array: "Gallery" is the new internal page, others use the external link.
 const navigation = [
   { name: "Gallery", href: "/gallery" },
   { name: "Music", href: "https://www.affirmamint.cloud" },
@@ -12,6 +12,8 @@ const navigation = [
 ];
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <div className="w-full">
       <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
@@ -34,31 +36,30 @@ const Navbar = () => {
                     <span>Affirm</span>
                   </span>
                 </Link>
-
+                {/* Back/Forward Buttons */}
+                <div className="flex space-x-2 ml-4">
+                  <button
+                    type="button"
+                    onClick={() => router.back()}
+                    className="px-3 py-1 text-gray-600 bg-gray-100 rounded hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
+                  >
+                    ← Back
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => router.forward && router.forward()}
+                    className="px-3 py-1 text-gray-600 bg-gray-100 rounded hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
+                  >
+                    Forward →
+                  </button>
+                </div>
+                {/* ...rest of your code */}
                 <Disclosure.Button
                   aria-label="Toggle Menu"
                   className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-200 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
                 >
-                  <svg
-                    className="w-6 h-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                  >
-                    {open ? (
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                      />
-                    ) : (
-                      <path
-                        fillRule="evenodd"
-                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                      />
-                    )}
-                  </svg>
+                  {/* ... */}
                 </Disclosure.Button>
-
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
                   <>
                     {navigation.map((item, index) => (
@@ -86,6 +87,24 @@ const Navbar = () => {
         {/* menu  */}
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
+            {/* Back/Forward buttons */}
+            <li className="mr-3 nav__item flex items-center space-x-2">
+              <button
+                type="button"
+                onClick={() => router.back()}
+                className="px-3 py-1 text-gray-600 bg-gray-100 rounded hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
+              >
+                ← Back
+              </button>
+              <button
+                type="button"
+                onClick={() => router.forward && router.forward()}
+                className="px-3 py-1 text-gray-600 bg-gray-100 rounded hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
+              >
+                Forward →
+              </button>
+            </li>
+            {/* Navigation links */}
             {navigation.map((item, index) => (
               <li className="mr-3 nav__item" key={index}>
                 <Link
@@ -106,7 +125,6 @@ const Navbar = () => {
           >
             K.i.t.e.NFT 1SOL_1JUP 4/24
           </Link>
-
           <ThemeChanger />
         </div>
       </nav>
