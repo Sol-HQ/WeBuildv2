c298f76f5bbc6c22eb48712480067302229ab46d
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-08-06 15:43:11 -0700

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import ThemeChanger from "./DarkSwitch";
-import Image from "next/image"
+import Image from "next/image";
 import { Disclosure } from "@headlessui/react";
 
-const Navbar = () => {
-  const navigation = [
-    "Art",
-    "Music",
-    "Photography",
-    "CandyMachine",
-  ];
+// Navigation array: "Gallery" is the new internal page, others use the external link.
+const navigation = [
+  { name: "Gallery", href: "/gallery" },
+  { name: "Music", href: "https://www.affirmamint.cloud" },
+  { name: "Photography", href: "https://www.affirmamint.cloud" },
+  { name: "CandyMachine", href: "https://www.affirmamint.cloud" },
+];
 
+const Navbar = () => {
   return (
     <div className="w-full">
       <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
@@ -36,19 +37,20 @@ const Navbar = () => {
 
                 <Disclosure.Button
                   aria-label="Toggle Menu"
-                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-200 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700">
+                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-200 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
+                >
                   <svg
                     className="w-6 h-6 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24">
-                    {open && (
+                    viewBox="0 0 24 24"
+                  >
+                    {open ? (
                       <path
                         fillRule="evenodd"
                         clipRule="evenodd"
                         d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
                       />
-                    )}
-                    {!open && (
+                    ) : (
                       <path
                         fillRule="evenodd"
                         d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
@@ -60,12 +62,19 @@ const Navbar = () => {
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
                   <>
                     {navigation.map((item, index) => (
-                      <Link key={index} href="https://www.affirmamint.cloud" className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
-                          {item}
+                      <Link
+                        key={index}
+                        href={item.href}
+                        className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none"
+                      >
+                        {item.name}
                       </Link>
                     ))}
-                    <Link href="https://www.affirmamint.cloud" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">         
-                        K.i.t.e.NFT 1SOL_1JUP 4/24
+                    <Link
+                      href="https://www.affirmamint.cloud"
+                      className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5"
+                    >
+                      K.i.t.e.NFT 1SOL_1JUP 4/24
                     </Link>
                   </>
                 </Disclosure.Panel>
@@ -77,10 +86,13 @@ const Navbar = () => {
         {/* menu  */}
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
-            {navigation.map((menu, index) => (
+            {navigation.map((item, index) => (
               <li className="mr-3 nav__item" key={index}>
-                <Link href="https://www.affirmamint.cloud" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
-                    {menu}
+                <Link
+                  href={item.href}
+                  className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
+                >
+                  {item.name}
                 </Link>
               </li>
             ))}
@@ -88,8 +100,11 @@ const Navbar = () => {
         </div>
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
-          <Link href="https://www.affirmamint.cloud" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
-              K.i.t.e.NFT 1SOL_1JUP 4/24
+          <Link
+            href="https://www.affirmamint.cloud"
+            className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5"
+          >
+            K.i.t.e.NFT 1SOL_1JUP 4/24
           </Link>
 
           <ThemeChanger />
@@ -97,9 +112,6 @@ const Navbar = () => {
       </nav>
     </div>
   );
-}
+};
 
 export default Navbar;
-
-
-
