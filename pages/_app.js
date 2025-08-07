88b5bc3fd4e440ae4811104e9f249a93ac6c7590
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-08-06 17:47:26 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
+import JupiterWidget from '../components/JupiterWidget';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <JupiterWidget />
     </ThemeProvider>
   );
 }
