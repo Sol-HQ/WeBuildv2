fd35054aec0f7086588011a3c5f85c488917ef10
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-08-06 17:40:57 -0700

    Update jupiterwidget.js

--- components/jupiterwidget.js
+++ components/jupiterwidget.js
@@ -1,1 +1,40 @@
+import Script from 'next/script'
+import { useEffect } from 'react'
 
+export default function JupiterWidget() {
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      if (typeof window.Jupiter !== 'undefined') {
+        window.Jupiter.init({
+          displayMode: 'widget',
+          endpoint: 'https://mainnet.helius-rpc.com/?api-key=6cf235c9-8b64-4e86-bede-fbf0fd688c9f',
+          containerName: 'jupiter-terminal',
+          refetchIntervalForTokenAccounts: 10,
+          strictTokenList: false,
+          formProps: {
+            fixedInputMint: true,
+            fixedOutputMint: true,
+            initialAmount: '1000000',
+            initialInputMint: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+            initialOutputMint: 'So11111111111111111111111111111111111111112',
+          },
+        })
+      }
+    })
+  }, [])
+
+  return (
+    <>
+      <Script src="https://terminal.jup.ag/main-v4.js" strategy="afterInteractive" />
+      <div
+        id="jupiter-terminal"
+        style={{
+          position: 'fixed',
+          bottom: 24,
+          left: 24,
+          zIndex: 10000,
+        }}
+      />
+    </>
+  )
+}
