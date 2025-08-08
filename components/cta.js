05efa2d058d5daa225847e7a666f2a94cddae93c
C.s.w. <104325729+Sol-HQ@users.noreply.github.com>
2025-08-07 22:23:51 -0700

    Update cta.js

--- components/cta.js
+++ components/cta.js
@@ -1,57 +1,42 @@
 import React from "react";
 import Container from "./container";
 //update august 5
+
 const Cta = () => {
   return (
     <Container>
-<Container>
-  <div className="flex flex-col w-full max-w-6xl mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 rounded-xl">
-    <div className="text-center lg:text-left">
-      <h2 className="text-2xl font-medium lg:text-3xl">
-        KITE NFT Collection Mint... is LIVE<br />
-        <br />
-        Pilot AKA "High Flyers" Coin... is LIVE
-      </h2>
-      <p className="mt-2 font-medium text-white text-opacity-90 lg:text-xl">
-        Limited Collection Of Digital Pilot Kite NFT's<br />
-        Mint & Tracking of PILOT Token<br />
-        EtYGHRkV9ZW7oAhNvspktvPdb4nxPq1UDBBNCpvwjups
-      </p>
-      <div className="flex flex-col gap-4 mt-8 items-center w-full">
-        <a
-          href="https://www.affirmamint.cloud"
-          target="_blank"
-          rel="noopener"
-          className="inline-block py-3 text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 w-full max-w-xs">
-          Kite NFT LIVE 1SOL 1JUP
-        </a>
-        <a
-          href="https://jup.ag/tokens/EtYGHRkV9ZW7oAhNvspktvPdb4nxPq1UDBBNCpvwjups"
-          target="_blank"
-          rel="noopener"
-          className="px-8 py-4 text-lg font-medium text-center text-white bg-blue-900 rounded-md w-full max-w-xs">
-          Pilot Coin Live
-        </a>
+      <div className="flex flex-col w-full max-w-6xl mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 rounded-xl">
+        <div className="text-center lg:text-left">
+          <h2 className="text-2xl font-medium lg:text-3xl">
+            KITE NFT Collection Mint... is LIVE<br />
+            <br />
+            Pilot AKA "High Flyers" Coin... is LIVE
+          </h2>
+          <p className="mt-2 font-medium text-white text-opacity-90 lg:text-xl">
+            Limited Collection Of Digital Pilot Kite NFT's<br />
+            Mint & Tracking of PILOT Token<br />
+            EtYGHRkV9ZW7oAhNvspktvPdb4nxPq1UDBBNCpvwjups
+          </p>
+          <div className="flex flex-col gap-4 mt-8 items-center w-full">
+            <a
+              href="https://www.affirmamint.cloud"
+              target="_blank"
+              rel="noopener"
+              className="inline-block py-3 text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 w-full max-w-xs">
+                Kite NFT LIVE 1SOL 1JUP
+            </a>
+            <a
+              href="https://jup.ag/tokens/EtYGHRkV9ZW7oAhNvspktvPdb4nxPq1UDBBNCpvwjups"
+              target="_blank"
+              rel="noopener"
+              className="px-8 py-4 text-lg font-medium text-center text-white bg-blue-900 rounded-md w-full max-w-xs">
+                Pilot Coin Live
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</Container>
-
+    </Container>
+  );
+};
 
 export default Cta;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
